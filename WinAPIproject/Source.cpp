c03597af2fd59ed1fe32942283c1eae41bf4e24a
lynnkse <lynnkse@gmail.com>
2015-05-13 16:57:18 +0300

    working with all three windows

--- WinAPIproject/Source.cpp
+++ WinAPIproject/Source.cpp
@@ -2,22 +2,23 @@
 // Store handles to the main window and application
 // instance globally.
 HWND ghMainWnd = 0;
+HWND secondWnd = 0;
+HWND thirdWnd = 0;
 HINSTANCE ghAppInst = 0;
-// Step 1: Define and implement the window procedure.
-LRESULT CALLBACK
-WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+// Step 1: Define and implement the window procedures.
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
 		// Handle left mouse button click message.
 	case WM_LBUTTONDOWN:
-		MessageBox(ghMainWnd, L"WM_LBUTTONDOWN message.", L"Msg", MB_OK);
+		MessageBox(ghMainWnd, L"You clicked the first window", L"Msg", MB_OK);
 		return 0;
 		// Handle key down message.
 	case WM_KEYDOWN:
 		if (wParam == VK_ESCAPE)
 		{
-			int ms = MessageBox(ghMainWnd, L"WM_LBUTTONDOWN message.", L"Msg", MB_YESNO);
+			int ms = MessageBox(ghMainWnd, L"Exit?", L"Msg", MB_YESNO);
 			if (ms == 6)
 				DestroyWindow(ghMainWnd);
 		}
@@ -32,12 +33,39 @@ WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
+LRESULT CALLBACK scndWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_LBUTTONDOWN:
+		MessageBox(secondWnd, L"You clicked on the second window", L"Msg", MB_OK);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+LRESULT CALLBACK thirdWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_LBUTTONDOWN:
+		MessageBox(thirdWnd, L"You clicked on the third window", L"Msg", MB_OK);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 // WinMain: Entry point for a Windows application.
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,PSTR cmdLine, int showCmd)
 {
 	// Save handle to application instance.
 	ghAppInst = hInstance;
-	// Step 2: Fill out a WNDCLASS instance.
+	// Step 2: Fill out a WNDCLASS instances.
 	WNDCLASS wc;
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.lpfnWndProc = WndProc;
@@ -49,9 +77,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,PSTR cmdLine, in
 	wc.hbrBackground = (HBRUSH)::GetStockObject(WHITE_BRUSH);
 	wc.lpszMenuName = 0;
 	wc.lpszClassName = L"MyWndClassName";
+	
+	WNDCLASS wc2;
+	wc2.style = CS_HREDRAW | CS_VREDRAW;
+	wc2.lpfnWndProc = scndWndProc;
+	wc2.cbClsExtra = 0;
+	wc2.cbWndExtra = 0;
+	wc2.hInstance = ghAppInst;
+	wc2.hIcon = ::LoadIcon(0, IDI_APPLICATION);
+	wc2.hCursor = ::LoadCursor(0, IDC_ARROW);
+	wc2.hbrBackground = (HBRUSH)::GetStockObject(WHITE_BRUSH);
+	wc2.lpszMenuName = 0;
+	wc2.lpszClassName = L"MyWndClassName2";
+	
+	WNDCLASS wc3;
+	wc3.style = CS_HREDRAW | CS_VREDRAW;
+	wc3.lpfnWndProc = thirdWndProc;
+	wc3.cbClsExtra = 0;
+	wc3.cbWndExtra = 0;
+	wc3.hInstance = ghAppInst;
+	wc3.hIcon = ::LoadIcon(0, IDI_APPLICATION);
+	wc3.hCursor = ::LoadCursor(0, IDC_ARROW);
+	wc3.hbrBackground = (HBRUSH)::GetStockObject(WHITE_BRUSH);
+	wc3.lpszMenuName = 0;
+	wc3.lpszClassName = L"MyWndClassName3";
 	// Step 3: Register the WNDCLASS instance with Windows.
 	RegisterClass(&wc);
-	// Step 4: Create the window, and save handle in globla
+	RegisterClass(&wc2);
+	RegisterClass(&wc3);
+	// Step 4: Create the windows, and save handle in globla
 	// window handle variable ghMainWnd.
 	ghMainWnd = ::CreateWindow(L"MyWndClassName", L"MyWindow", WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL, 0, 0, 500, 500, 0, 0, ghAppInst, 0);
 	if (ghMainWnd == 0)
@@ -59,9 +113,29 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,PSTR cmdLine, in
 		::MessageBox(0, L"CreateWindow - Failed", 0, 0);
 		return false;
 	}
-	// Step 5: Show and update the window.
+	
+	secondWnd = ::CreateWindow(L"MyWndClassName2", L"MyWindow2", WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL, 550, 0, 500, 500, 0, 0, ghAppInst, 0);
+	if (secondWnd == 0)
+	{
+		::MessageBox(0, L"CreateWindow(2) - Failed", 0, 0);
+		return false;
+	}
+
+	thirdWnd = ::CreateWindow(L"MyWndClassName3", L"MyWindow3", WS_OVERLAPPEDWINDOW | WS_HSCROLL | WS_VSCROLL, 550, 0, 250, 250, 0, 0, ghAppInst, 0);
+	if (thirdWnd == 0)
+	{
+		::MessageBox(0, L"CreateWindow(3) - Failed", 0, 0);
+		return false;
+	}
+	// Step 5: Show and update the windows.
 	ShowWindow(ghMainWnd, showCmd);
 	UpdateWindow(ghMainWnd);
+	
+	ShowWindow(secondWnd, showCmd);
+	UpdateWindow(secondWnd);
+	
+	ShowWindow(thirdWnd, showCmd);
+	UpdateWindow(thirdWnd);
 	// Step 6: Enter the message loop and don't quit until
 	// a WM_QUIT message is received.
 	MSG msg;
